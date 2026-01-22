2258bc788ad5c75a09e0b49aff4b4ae4e5d9bb66
Jaylingers001 <bjomariejay@gmail.com>
2026-01-22 11:02:11 +0800

    add header, main and footer into app.ts

--- ts-fe-angular-app/src/app/app.ts
+++ ts-fe-angular-app/src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
+import { HeaderComponent } from './components/header-component/header-component';
+import { FooterComponent } from './components/footer-component/footer-component';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home-component/home-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [HeaderComponent, FooterComponent, HomeComponent],
+  // templateUrl: './app.html',
+  styleUrl: './app.css',
+  template: `
+    <app-header-component/>
+    <main >
+      <app-home-component/>
+    </main>
+    <app-footer-component/>
+
+  `
 })
 export class App {
-  protected readonly title = signal('ts-fe-angular-app');
+  protected readonly title = signal('angular-tailwind-app');
 }
