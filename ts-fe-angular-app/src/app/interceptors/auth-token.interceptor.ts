ddb1dfc3ae1d2638c4d725042af76d5c3d146c40
Jaylingers001 <bjomariejay@gmail.com>
2026-01-23 10:17:19 +0800

    Add interceptors for the auth-token. This ensures that if a token exists, the user will remain on the same page even after refreshing.

---
+++ ts-fe-angular-app/src/app/interceptors/auth-token.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { getStoredToken } from '../utils/token-storage';
+
+export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
+  const token = getStoredToken();
+
+  if (!token) {
+    alert(12121)
+    return next(req);
+  }
+
+  const authorizedReq = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  return next(authorizedReq);
+};
